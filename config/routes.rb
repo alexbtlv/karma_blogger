efacfeb2a276498f1bbaa4e14cd928b0a9ca4b1c
Alexander Batalov <alexbtlv@gmail.com>
2014-07-26 01:31:23 +0600

    setup Foundation and rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "home#index"
+  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'verification', registration: 'register', sign_up: 'cmon_let_me_in' }
+  root to: "posts#index"
 end
