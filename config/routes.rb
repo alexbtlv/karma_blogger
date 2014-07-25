d94fd5ae721a760d62eeb2aeb8d1d162b41a92b8
Alexander Batalov <alexbtlv@gmail.com>
2014-07-26 03:07:55 +0600

    we are getting closer

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'password', confirmation: 'verification', registration: 'register', sign_up: 'cmon_let_me_in' }
   root to: "posts#index"
+  resources :posts
 end
