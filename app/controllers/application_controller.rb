60b23b71d55365b42fb69d2f4d85f91340287814
Alexander Batalov <alexbtlv@gmail.com>
2014-07-26 13:59:04 +0600

    Registration and sign in mechanics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 
 end
