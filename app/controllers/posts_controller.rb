715acb1f1999220c1cfdc6711dedb1d8d9ce762b
Alexander Batalov <alexbtlv@gmail.com>
2014-07-27 16:56:50 +0600

    updated edit article page to meet new form designs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
 
 		respond_to do |format|
     		if @post.save
-        		format.html { redirect_to @post, notice: 'post was successfully created.' }
+        		format.html { redirect_to @post, notice: 'Article was successfully created.' }
         		format.json { render :show, status: :created, location: @post }
       		else
         		format.html { render :new }
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
 	def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'post was successfully updated.' }
+        format.html { redirect_to @post, notice: 'Article was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: 'Article was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
