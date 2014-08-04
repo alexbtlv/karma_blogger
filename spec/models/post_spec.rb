fa8b09333e0f2fbc6f7e27d6b815eac4bed0788d
Alexander Batalov <alexbtlv@gmail.com>
2014-08-04 12:38:22 +0600

    updated model specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,6 +9,10 @@ describe Post  do
 	it { is_expected.to respond_to(:title)}
   it { is_expected.to respond_to(:body)}
   it { is_expected.to respond_to(:user)}
+  it { is_expected.to respond_to(:cached_votes_total)}
+  it { is_expected.to respond_to(:cached_votes_score)}
+  it { is_expected.to respond_to(:cached_votes_up)}
+  it { is_expected.to respond_to(:cached_votes_down)}
   it { is_expected.to be_valid }
 
 	describe '#user' do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,100 +2,97 @@ require 'spec_helper'
 
 describe User do
 	let(:valid_attributes) {
-	    {
-	      name: "Alexander",
-	      email: "alexander@karma.com",
-	      password: "1234567890",
-	      password_confirmation: "1234567890"
-	    }
+    {
+      name: "Alexander",
+      email: "alexander@karma.com",
+      password: "1234567890",
+      password_confirmation: "1234567890"
+    }
 	}
 
-  	context "validations" do
-	  	let(:user) { User.new(valid_attributes) }
+  it { is_expected.to respond_to(:name)}
+  it { is_expected.to respond_to(:email)}
+  it { is_expected.to respond_to(:password)}
+  it { is_expected.to respond_to(:password_confirmation)}
 
-	  	before do
-	      User.create(valid_attributes)
-	    end
 
-	    it "requires an name" do 
-	      expect(user).to validate_presence_of(:name)
-	    end
+	context "validations" do
+  	let(:user) { User.new(valid_attributes) }
 
-	    it "requires a unique name" do
-	      expect(user).to validate_uniqueness_of(:name)
-	    end
+  	before do
+      User.create(valid_attributes)
+    end
 
-	    it "requires an email" do 
-	      expect(user).to validate_presence_of(:email)
-	    end
+    it "requires an name" do 
+      expect(user).to validate_presence_of(:name)
+    end
 
-	    it "requires a unique email" do
-	      expect(user).to validate_uniqueness_of(:email)
-	    end
+    it "requires a unique name" do
+      expect(user).to validate_uniqueness_of(:name)
+    end
 
-	    it "requires a unique email and case insensitive" do
-	      user.email = "ALEXANDER@KARMA.COM"
-	      expect(user).to validate_uniqueness_of(:email)
-	    end
+    it "requires an email" do 
+      expect(user).to validate_presence_of(:email)
+    end
 
-	    it "requires the email address to look like an email" do
-	      user.email = "12345"
-	      expect(user).to_not be_valid
-	    end
+    it "requires a unique email" do
+      expect(user).to validate_uniqueness_of(:email)
+    end
 
-	    it "should have a password attribute" do
-      		expect(user).to respond_to(:password)
-    	end
+    it "requires a unique email and case insensitive" do
+      user.email = "ALEXANDER@KARMA.COM"
+      expect(user).to validate_uniqueness_of(:email)
+    end
 
-	    it "should have a password confirmation attribute" do
-	      expect(user).to respond_to(:password_confirmation)
-	    end
-  	end
-
-  	describe "downcase name" do
-		it "makes the name attribute lower case" do
-			user = User.new(valid_attributes.merge(name: "ALEXANDER"))
-      		expect{ user.downcase_name }.to change{ user.name }.
-        		from("ALEXANDER").
-       			to("alexander")
-		end
-
-		it "downcases an name before saving" do
-		    user = User.new(valid_attributes)
-		    user.name = "OBAMA"
-		    expect(user.save).to be_truthy
-		    expect(user.name).to eq("obama")
-    	end
+    it "requires the email address to look like an email" do
+      user.email = "12345"
+      expect(user).to_not be_valid
+    end
 	end
 
-	describe "downcase email" do
-		it "makes the email attribute lower case" do
-			user = User.new(valid_attributes.merge(email: "ALEXANDER@KARMA.COM"))
-      		expect{ user.downcase_email }.to change{ user.email }.
-        		from("ALEXANDER@KARMA.COM").
-       			to("alexander@karma.com")
-		end
-
-		it "downcases an email before saving" do
-		    user = User.new(valid_attributes)
-		    user.email = "OBAMA@KARMA.COM"
-		    expect(user.save).to be_truthy
-		    expect(user.email).to eq("obama@karma.com")
-    	end
+	describe "downcase name" do
+	it "makes the name attribute lower case" do
+		user = User.new(valid_attributes.merge(name: "ALEXANDER"))
+    		expect{ user.downcase_name }.to change{ user.name }.
+      		from("ALEXANDER").
+     			to("alexander")
 	end
 
-	context "relationships" do
-	    it { should have_many(:posts) }
+	it "downcases an name before saving" do
+    user = User.new(valid_attributes)
+    user.name = "OBAMA"
+    expect(user.save).to be_truthy
+    expect(user.name).to eq("obama")
+	end
+end
 
-	    it "should destroy associated posts" do
-	    	user = User.new(valid_attributes)
-		    posts = user.posts.dup
-		    user.destroy
-		    expect(posts).to be_empty
-		    posts.each do |post|
-		    	expect(Post.find_by_id(post.id)).to be_nil
-		    end
+describe "downcase email" do
+	it "makes the email attribute lower case" do
+		user = User.new(valid_attributes.merge(email: "ALEXANDER@KARMA.COM"))
+		expect{ user.downcase_email }.to change{ user.email }.
+      		from("ALEXANDER@KARMA.COM").to("alexander@karma.com")
+	end
+
+	it "downcases an email before saving" do
+    user = User.new(valid_attributes)
+    user.email = "OBAMA@KARMA.COM"
+    expect(user.save).to be_truthy
+    expect(user.email).to eq("obama@karma.com")
+	end
+end
+
+context "relationships" do
+    it { should have_many(:posts) }
+
+    it "should destroy associated posts" do
+    	user = User.new(valid_attributes)
+	    posts = user.posts.dup
+	    user.destroy
+	    expect(posts).to be_empty
+	    posts.each do |post|
+	    	expect(Post.find_by_id(post.id)).to be_nil
 	    end
+    end
 	end
 end
 
