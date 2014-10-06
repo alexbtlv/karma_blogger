aa4c6e262cb27522345a35f0e8ed96a4e99bf153
Alexander Batalov <alexbtlv@gmail.com>
2014-10-07 01:38:07 +0600

    fixed routing tests

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -3,43 +3,43 @@ require "spec_helper"
 describe PostsController do
 	describe "routing" do
 		it "routes to #best" do
-			get("/").should route_to("posts#best")
+			expect(get("/")).to route_to("posts#best")
 		end
 
 		it "routes to #fresh" do
-			get("/fresh").should route_to("posts#fresh")
+			expect(get("/fresh")).to  route_to("posts#fresh")
 		end
 
 		it "routes to #show" do
-			get("/slug").should route_to("posts#show", :id => 'slug')
+			expect(get("/slug")).to route_to("posts#show", :id => 'slug')  
 		end
 
 		it "routes to #new" do
-			get("/new-story").should route_to("posts#new")
+			expect(get("/new-story")).to route_to("posts#new")
 		end
 
 		it "routes to #edit" do
-			get("/slug/edit").should route_to("posts#edit", :id => 'slug')
+			expect(get("/slug/edit")).to  route_to("posts#edit", :id => 'slug')
 		end
 
 		it "routes to #create" do
-			post("/").should route_to("posts#create")
+			expect(post("/")).to route_to("posts#create")
 		end
 
 		it "routes to #update" do
-			put("/slug").should route_to("posts#update", :id => 'slug')
+			expect(put("/slug")).to route_to("posts#update", :id => 'slug')
 		end
 
 		it "routes to #destroy" do
-			delete("/slug").should route_to("posts#destroy", :id => 'slug')
+			expect(delete("/slug")).to  route_to("posts#destroy", :id => 'slug')
 		end
 
 		it "routes to #karma_up" do
-			patch("/slug/karma_up").should route_to("posts#karma_up", :id => 'slug')
+			expect(patch("/slug/karma_up")).to   route_to("posts#karma_up", :id => 'slug')
 		end
 
 		it "routes to #karma_down" do
-			patch("/slug/karma_down").should route_to("posts#karma_down", :id => 'slug')
+			expect(patch("/slug/karma_down")).to  route_to("posts#karma_down", :id => 'slug')
 		end
 	end
 end
